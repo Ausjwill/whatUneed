4656f52d79f342f7f7852b47e6e25fbe2307a77b
Austin <ausjwill@gmail.com>
2020-11-15 14:29:48 -0500

    layout_updates

---
+++ whatUneed.Models/Emotional.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whatUneed.Models
+{
+    class Emotional
+    {
+        public int EmotionalID { get; set; }
+
+    }
+}
